8b4b460e4fe6b16e76f5248fedb961014f36b1fe
David Rovani <david@rovani.net>
2022-08-05 17:07:56 -0500

    Set-up from drovani-sandbox storefront.

--- hydrogen.config.ts
+++ hydrogen.config.ts
@@ -1,22 +1,26 @@
 import {
-  defineConfig,
   CookieSessionStorage,
-  PerformanceMetricsServerAnalyticsConnector,
+  defineConfig,
+  PerformanceMetricsServerAnalyticsConnector
 } from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: () => ({
     defaultLanguageCode: 'EN',
     defaultCountryCode: 'US',
-    storeDomain: Oxygen.env.SHOPIFY_STORE_DOMAIN,
-    storefrontToken: Oxygen.env.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN,
+    storeDomain:
+      Oxygen.env.SHOPIFY_STORE_DOMAIN || 'drovani-sandboxstore.myshopify.com',
+    storefrontToken:
+      Oxygen.env.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      'f799cf8e11b00c88e674228410422ffd',
     storefrontApiVersion: '2022-07',
   }),
+  routes: '/src/routes',
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    //sameSite: 'strict',
     maxAge: 60 * 60 * 24 * 30,
   }),
   serverAnalyticsConnectors: [PerformanceMetricsServerAnalyticsConnector],
